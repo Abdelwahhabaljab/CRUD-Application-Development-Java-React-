42153af88054eb937ff4249d55d546fc5af5c38c
Abdelwahhab <aljabouri2000@gmail.com>
2025-06-24 21:01:11 +0200

    feat: add Product entity

---
+++ crudbackend/src/main/java/com/example/crudbackend/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.crudbackend.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @Min(0)
+    private double price;
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+}
